d5c0e00e3b3a02352c8fe48829c13ceda39487c2
JP Salo <jpsalo@zoho.com>
2019-03-14 18:35:42 +0200

    Logging

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const reservationsRouter = require('./controllers/reservations')
 
-// logger.info('connecting to', config.MONGODB_URI)
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
