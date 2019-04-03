stdty-qrp/stdy-qrp-backend

commit 2b79ca012ec85bc8e7695a2e2ca71d62c5da7025

Use dev db in dev env

utils/config.js
@@ -2,12 +2,16 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-let PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
+if (process.env.NODE_ENV === 'development') {
+  MONGODB_URI = process.env.DEV_MONGODB_URI
+}
 
 if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI
 }
+
+let PORT = process.env.PORT
+let MONGODB_URI = process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,

commit f38361594fb022e9014fa77483aec0bd1c7224a5

Fix setting the db correctly. Broke in 2b79ca012ec85bc8e7695a2e2ca71d62c5da7025

utils/config.js
@@ -2,6 +2,9 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+let MONGODB_URI = process.env.MONGODB_URI
+let PORT = process.env.PORT
+
 if (process.env.NODE_ENV === 'development') {
   MONGODB_URI = process.env.DEV_MONGODB_URI
 }
@@ -9,9 +12,6 @@ if (process.env.NODE_ENV === 'development') {
 if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI
 }
-
-let PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,